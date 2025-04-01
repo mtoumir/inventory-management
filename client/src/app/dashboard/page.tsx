66a6ad68ec0dd08311785eefd8df40f7c7b69e24
med toumir <mtoumir@gmail.com>
2025-04-01 21:46:24 +0000

    initila setup, sidebar, navbar

---
+++ client/src/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import {
+  CheckCircle,
+  Package,
+  Tag,
+  TrendingDown,
+  TrendingUp,
+} from "lucide-react";
+
+
+const Dashboard = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      dashboard
+    </div>
+  );
+};
+
+export default Dashboard;
